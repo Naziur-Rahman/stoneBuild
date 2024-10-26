a6a99418af7e6af224803f883fde40c2b43edb0c
Naziur-Rahman <naziurr85@gmail.com>
2024-10-26 21:08:26 +0600

    React Icon Install

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import Layout from './Components/Layout/Layout';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
 const router = createBrowserRouter([
   {
     path: "/",element:<Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,children: [
+      {
+        path: "/",element: <Home></Home>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById('root')).render(
